ad85837ff15e4b29fa18c49d96722b1eb04bd1af
tAf <muzirat0@gmail.com>
2024-02-28 22:43:42 +0200

    checks if a binary tree is perfect v0.1

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks tree symetry
+ * @tree: Input, tree
+ *
+ * Return: status
+ *
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_is_perfect(tree->left) + 1;
+	right_height = binary_tree_is_perfect(tree->right) + 1;
+	if (left_height == right_height)
+		return (1);
+	else
+		return (0);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -44,5 +45,6 @@ size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
