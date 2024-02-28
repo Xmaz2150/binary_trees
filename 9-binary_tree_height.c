860eab5b6cd552828a88927700151177d99bf4c3
tAf <muzirat0@gmail.com>
2024-02-28 21:59:19 +0200

    measure height v2
    goes through both left and right branches

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,20 +5,19 @@
  * @tree: Input, tree
  *
  * Return: height
+ *
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	const binary_tree_t *tmp;
-	size_t height = 0;
+	size_t left_height, right_height;
 
 	if (tree == NULL)
-		return (0);
-	tmp = tree;
-	while (tmp->right != NULL)
-	{
-		height++;
-		tmp = tmp->right;
-	}
-
-	return (height);
+		return (-1);
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
+
