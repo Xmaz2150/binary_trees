03ab80b11ef93357e25dac22133db664c60bbf93
tAf <muzirat0@gmail.com>
2024-02-29 16:29:32 +0200

    back to v1

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,7 +12,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int left_height, right_height;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	left_height = binary_tree_is_perfect(tree->left) + 1;
 	right_height = binary_tree_is_perfect(tree->right) + 1;
 	if (left_height == right_height)
