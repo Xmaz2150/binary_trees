626bb2f782effb24cd992ab3e6085b052dd7b125
tAf <muzirat0@gmail.com>
2024-02-29 16:43:27 +0200

    header

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,3 +16,47 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	else
 		return (binary_tree_sibling(node->parent));
 }
+
+/**
+ * binary_tree_sibling - returns sibling of node
+ * @node: Input, node
+ *
+ * Return: sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (node == NULL)
+		return (NULL);
+
+	parent = node->parent;
+	if (parent == NULL)
+		return (NULL);
+
+	if (parent->right)
+	{
+		if (parent->left)
+		{
+			if (parent->left->n == node->n)
+			{
+				if (parent->right)
+					return (parent->right);
+			}
+			else
+				return (parent->left);
+		}
+		else
+			return (NULL);
+	}
+	else if (parent->left)
+	{
+		if (parent->right)
+			return (parent->right);
+		else
+			return (NULL);
+	}
+
+	return (NULL);
+
+}
